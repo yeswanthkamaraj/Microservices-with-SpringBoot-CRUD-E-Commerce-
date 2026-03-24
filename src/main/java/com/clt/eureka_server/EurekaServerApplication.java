6715ee548516ad67abbdd360e69f29df4105fde6
yeswanthkamaraj <yeswanthkamaraj25@gmail.com>
2026-03-24 13:08:39 +0530

    Configured the code with eureka server and able to view the eurekadashboard

--- src/main/java/com/clt/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/clt/eureka_server/EurekaServerApplication.java
@@ -2,11 +2,14 @@ package com.clt.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 

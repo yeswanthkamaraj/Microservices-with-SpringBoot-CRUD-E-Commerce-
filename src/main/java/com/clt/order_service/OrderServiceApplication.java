6d16ac123817f171c7c4f0f7e7559c7cb8bb3736
yeswanthkamaraj <yeswanthkamaraj25@gmail.com>
2026-03-25 15:13:23 +0530

    intial commit

---
+++ src/main/java/com/clt/order_service/OrderServiceApplication.java
@@ -0,0 +1,16 @@
+package com.clt.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
